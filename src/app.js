a96e2fc3fc7a94d27009b40bca256fc940e2a120
Natti94 <natnael_94@hotmail.com>
2025-01-17 10:11:18 +0100

    Seperated server from app app.js: - removed server. - added routes for user, messages & auth.
    server.js:
    - added listener for port.
    - hidden :port processing from env file.

--- src/app.js
+++ src/app.js
@@ -1,31 +1,20 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();  
+require("dotenv").config(); 
+
+const userRoutes = require("./routes/users");
+const authRoutes = require("./routes/auth");
+const messageRoutes = require("./routes/messages");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
-
-
 app.use(cors());  
-app.use(express.json());  
+app.use(express.json()); 
 
 
-app.get("/", (req, res) => {
-  res.send("Välkommen till Express-servern!");
-});
+app.use('/api/users')
+app.use('/api/auth')
+app.use('/api/messages')
+
+module.exports = app;
 
 
-app.use((req, res) => {
-  res.status(404).json({ message: "Route not found." });
-});
-
-
-app.use((err, req, res, next) => {
-  console.error(err);  
-  res.status(500).json({ message: "Internal server error." });
-});
-
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});

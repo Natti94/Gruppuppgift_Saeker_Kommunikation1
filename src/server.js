642f83614e625b2e6e16822345fdef71ef248ecf
jonatangroncode <jonatan.gron.code@hotmail.com>
2025-01-29 21:29:48 +0100

    Changing http server to redirect to safe https server

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
-const app = require("./app");
+const http = require("http");
+const app = require("./app"); 
 
-const PORT = process.env.PORT || 5000;
+const HTTP_PORT = process.env.PORT || 5000;  
+const HTTPS_PORT = process.env.SECURE_PORT || 5001; 
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+http.createServer((req, res) => {
+  const newHost = req.headers.host.replace(`:${HTTP_PORT}`, `:${HTTPS_PORT}`); 
+  res.writeHead(301, { Location: `https://${newHost}${req.url}` });
+  res.end();
+}).listen(HTTP_PORT, () => {
+  console.log(`HTTP Server running on port ${HTTP_PORT}, redirecting to HTTPS on port ${HTTPS_PORT}`);
 });
